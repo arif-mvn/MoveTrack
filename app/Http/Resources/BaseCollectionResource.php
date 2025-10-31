cb65a0822eaf9aa4e4873249f5901b79879c5db4
arif-mvn <ariful.islam@moveon.global>
2025-11-01 02:30:31 +0600

    create base collection resource

---
+++ app/Http/Resources/BaseCollectionResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace MoveOn\Core\Http\Resources;
+
+use Illuminate\Http\Resources\Json\PaginatedResourceResponse;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class BaseCollectionResource extends ResourceCollection
+{
+    // the tap returns our new CustomMetaPaginationCollection
+    public static function collection($resource) {
+        return tap(new BasePaginatedResourceResponse($resource), function ($collection) {
+            if (property_exists(static::class, 'preserveKeys')) {
+                $collection->preserveKeys = (new static([]))->preserveKeys === true;
+            }
+        });
+    }
+
+    /**
+     * Create a paginate-aware HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function preparePaginatedResponse($request)
+    {
+        if ($this->preserveAllQueryParameters) {
+            $this->resource->appends($request->query());
+        } elseif (! is_null($this->queryParameters)) {
+            $this->resource->appends($this->queryParameters);
+        }
+
+        return (new BasePaginatedResourceResponse($this))->toResponse($request);
+    }
+
+    public function toArray($request) {
+        return parent::toArray($request);
+    }
+}
---
+++ app/Http/Resources/BasePaginatedResourceResponse.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace MoveOn\Core\Http\Resources;
+
+use Illuminate\Http\Resources\Json\PaginatedResourceResponse;
+use Illuminate\Support\Arr;
+
+class BasePaginatedResourceResponse extends PaginatedResourceResponse
+{
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function toResponse($request)
+    {
+        return tap(response()->json(
+            $this->wrap(
+                $this->resource->resolve($request),
+                array_merge_recursive(
+                    $this->paginationInformation($request),
+                    $this->resource->with($request),
+                    $this->resource->additional
+                )
+            ),
+            $this->calculateStatus(),
+            [],
+            $this->resource->jsonOptions()
+        ), function ($response) use ($request) {
+            $response->original = $this->resource->resource->map(function ($item) {
+                return is_array($item) ? Arr::get($item, 'resource') : $item->resource;
+            });
+
+            $this->resource->withResponse($request, $response);
+        });
+    }
+
+    /**
+     * Add the pagination information to the response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function paginationInformation($request)
+    {
+        $paginated = $this->resource->resource->toArray();
+        $paginationArray          = $this->meta($paginated);
+        $paginationArray["links"] = $this->paginationLinks($paginated);
+        if ($request->has("stringify_pagination")) {
+            $paginationArray["current_page"] = (string) $paginationArray["current_page"];
+        }
+        $default = [
+            'pagination' => $paginationArray,
+        ];
+
+        if (method_exists($this->resource, 'paginationInformation') ||
+            $this->resource->hasMacro('paginationInformation')) {
+            return $this->resource->paginationInformation($request, $paginated, $default);
+        }
+
+        return $default;
+    }
+
+    /**
+     * Get the pagination links for the response.
+     *
+     * @param  array  $paginated
+     * @return array
+     */
+    protected function paginationLinks($paginated)
+    {
+        return [
+            'first' => $paginated['first_page_url'] ?? null,
+            'last' => $paginated['last_page_url'] ?? null,
+            'prev' => $paginated['prev_page_url'] ?? null,
+            'next' => $paginated['next_page_url'] ?? null,
+        ];
+    }
+
+    /**
+     * Gather the meta data for the response.
+     *
+     * @param  array  $paginated
+     * @return array
+     */
+    protected function meta($paginated)
+    {
+        return Arr::except($paginated, [
+            'data',
+            'first_page_url',
+            'last_page_url',
+            'prev_page_url',
+            'next_page_url',
+        ]);
+    }
+}
